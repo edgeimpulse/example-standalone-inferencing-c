bd8202e40293758445e12ce1a9640e46ed204900
Jan Jongboom <janjongboom@gmail.com>
2020-12-23 12:47:18 +0100

    Update to latest SDK release and standalone version

--- source/main.c
+++ source/main.c
@@ -13,18 +13,53 @@ int get_feature_data(size_t offset, size_t length, float *out_ptr) {
     return 0;
 }
 
+int read_features_file(const char *filename) {
+    char buffer[1 * 1024 * 1024] = { 0 };
+    FILE *f = (FILE*)fopen(filename, "r");
+    if (!f) {
+        printf("Cannot open file %s\n", filename);
+        return -1;
+    }
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
+    rewind(f);
+    fread(buffer, 1, size, f);
+    fclose(f);
+
+    char *token = strtok(buffer, ",");
+    while (token != NULL) {
+        features[feature_ix++] = strtof(token, NULL);
+        token = strtok(NULL, " ");
+    }
+    return 0;
+}
+
 EI_IMPULSE_ERROR run_classifier(signal_t *, ei_impulse_result_t *, bool);
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        printf("Requires one parameter (a comma-separated list of raw features)\n");
+        printf("Requires one parameter (a comma-separated list of raw features, or a file pointing at raw features)\n");
         return 1;
     }
 
-    char *token = strtok(argv[1], ",");
-    while (token != NULL) {
-        features[feature_ix++] = strtof(token, NULL);
-        token = strtok(NULL, " ");
+    if (!strchr(argv[1], ' ') && strchr(argv[1], '.')) { // looks like a filename
+        int r = read_features_file(argv[1]);
+        if (r != 0) {
+            return 1;
+        }
+    }
+    else { // looks like a features array passed in as an argument
+        char *token = strtok(argv[1], ",");
+        while (token != NULL) {
+            features[feature_ix++] = strtof(token, NULL);
+            token = strtok(NULL, " ");
+        }
+    }
+
+    if (feature_ix != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
+        printf("The size of your 'features' array is not correct. Expected %d items, but had %lu\n",
+            EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, feature_ix);
+        return 1;
     }
 
     signal_t signal;
